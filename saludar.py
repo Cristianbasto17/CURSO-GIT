e69de76e451aebe2daeee30031995f3e327ba635
Cristianbasto17 <cdbastot@gmail.com>
2025-11-28 19:21:38 -0500

    Imprimiendo

---
+++ saludar.py
@@ -0,0 +1,9 @@
+print("=== BIENVENIDO ===\n")
+
+nombre = input("¿Cómo te llamas? ")
+edad = input("¿Cuántos años tienes? ")
+ciudad = input("¿De dónde eres? ")
+
+print(f"\n¡Hola {nombre}!")
+print(f"Es un placer conocerte. Veo que tienes {edad} años y eres de {ciudad}.")
+print(f"¡Espero que tengas un excelente día!")
